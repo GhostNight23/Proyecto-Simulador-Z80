754bd9a0ed7f5bc305078ba890eb4dcc7d57a135
Ricardo-Avila <54330605+Ricardo-Avila@users.noreply.github.com>
2022-05-30 19:46:12 -0500

    Create Comandos.java

---
+++ Comandos.java
@@ -0,0 +1,46 @@
+package Archivos;
+
+/**
+ *
+ * @author Ricardo
+ */
+public class Comandos {
+    
+    public String instruccion;
+    public int codigo_instruccion;
+    public String A;
+    public String B;
+    
+    public Comandos(String instruccion, int codigo_instruccion , String A, String B){
+        this.instruccion = instruccion;
+        this.codigo_instruccion = codigo_instruccion;
+        this.A = A;
+        this.B = B;
+    }
+    
+    public Comandos(String instruccion,String A, String B){
+        this.instruccion = instruccion;
+        this.A = A;
+        this.B = B;
+    }
+    
+    public int LD (int A, int B){
+        int resultado=0;
+        
+        //Proceso
+        
+        return resultado;
+    }
+    
+    /*public static int Hex_Dec(String hexadecimal){
+        int decimal;
+        //Proceso
+        return decimal;
+    }
+    
+    public static String Dec_Hex(int Decimal){
+        String hexadecimal;
+        //Proceso
+        return hexadecimal;
+    }*/
+}
